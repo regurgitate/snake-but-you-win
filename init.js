7271a3ff1d554b156db1ef339da91a025f02bd22
Krzysztof <olejnik.contact@gmail.com>
2023-07-07 19:38:32 +0200

    an apple on the board

--- init.js
+++ init.js
@@ -1,7 +1,12 @@
 const game = document.querySelector('.game');
+
+// board size
 const xMax = 30;
 const yMax = 30;
+
+// determining starting point (center of the board)
 let startingPoint = {};
+
 function determStartPoint(value, dim) {
   if (value % 2 === 0) {
     startingPoint[dim] = value / 2;
@@ -13,6 +18,7 @@ function determStartPoint(value, dim) {
 determStartPoint(xMax, 'x');
 determStartPoint(yMax, 'y');
 
+// setting up the board
 for (let i = 1; i <= xMax; i++) {
   for (let j = 1; j <= yMax; j++) {
     const div = document.createElement("div");
@@ -22,22 +28,49 @@ for (let i = 1; i <= xMax; i++) {
   }
 };
 
+// putting head at the starting point
 const divSelector = function(object) {
   return document.querySelector(`div[data-x="${object.x}"][data-y="${object.y}"]`);
 };
-const startDiv = divSelector(startingPoint);
-startDiv.classList.add("head-down");
-let headPos = {
-  x: startDiv.dataset.x,
-  y: startDiv.dataset.y,
-};
-let headDiv = startDiv;
 
-let mechanics = function(e) {
+let headDiv = divSelector(startingPoint);
+headDiv.classList.add("head-down");
+let headPos = {...startingPoint};
+
+// placing an apple
+let appleDiv = null;
+
+const placeApple = function() {
+  const rnd = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  };
+  let applePos = {
+    x: null,
+    y: null
+  };
+  const genAppleRndPos = function() {
+    applePos = {
+      x: rnd(1, xMax),
+      y: rnd(1, yMax)
+    };
+  };
+
+  do  {
+    genAppleRndPos();
+    appleDiv = divSelector(applePos);
+  }
+  while (appleDiv.className);
+
+  appleDiv.classList.add("apple");
+};
+
+// game mechanics (moving)
+const mechanics = function(e) {
   let clearAndReselect = function() {
     if (headPos.x <= 0 || headPos.x > xMax || headPos.y <= 0 || headPos.y > yMax) {
       alert('dead');
     };
+
     headDiv.className = '';
     headDiv = divSelector(headPos);
   };
@@ -61,4 +94,6 @@ let mechanics = function(e) {
   };
 };
 
+// start the game
+placeApple();
 document.addEventListener("keydown", mechanics);
