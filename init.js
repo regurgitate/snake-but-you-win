02b17baafdeeb47c8874cb0a8dd9b7c03a048599
Krzysztof <olejnik.contact@gmail.com>
2023-07-07 17:09:52 +0200

    head is moving

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 const game = document.querySelector('.game');
-const x = 30;
-const y = 30;
+const xMax = 30;
+const yMax = 30;
 let startingPoint = {};
 function determStartPoint(value, dim) {
   if (value % 2 === 0) {
@@ -10,17 +10,55 @@ function determStartPoint(value, dim) {
   }
 };
 
-determStartPoint(x, 'x');
-determStartPoint(y, 'y');
+determStartPoint(xMax, 'x');
+determStartPoint(yMax, 'y');
 
-for (let i = 1; i <= x; i++) {
-  for (let j = 1; j <= y; j++) {
+for (let i = 1; i <= xMax; i++) {
+  for (let j = 1; j <= yMax; j++) {
     const div = document.createElement("div");
-    div.setAttribute("data-x", i);
-    div.setAttribute("data-y", j);
+    div.setAttribute("data-x", j);
+    div.setAttribute("data-y", i);
     game.appendChild(div);
   }
 };
 
-const startDiv = document.querySelector(`div[data-x="${startingPoint.x}"][data-y="${startingPoint.y}"]`);
-startDiv.classList.add("head-left");
+const divSelector = function(object) {
+  return document.querySelector(`div[data-x="${object.x}"][data-y="${object.y}"]`);
+};
+const startDiv = divSelector(startingPoint);
+startDiv.classList.add("head-down");
+let headPos = {
+  x: startDiv.dataset.x,
+  y: startDiv.dataset.y,
+};
+let headDiv = startDiv;
+
+let mechanics = function(e) {
+  let clearAndReselect = function() {
+    if (headPos.x <= 0 || headPos.x > xMax || headPos.y <= 0 || headPos.y > yMax) {
+      alert('dead');
+    };
+    headDiv.className = '';
+    headDiv = divSelector(headPos);
+  };
+
+  if (e.key === "ArrowLeft") {
+    headPos.x--;
+    clearAndReselect();
+    headDiv.classList.add("head-left");
+  } else if (e.key === "ArrowRight") {
+    headPos.x++;
+    clearAndReselect();
+    headDiv.classList.add("head-right");
+  } else if (e.key === "ArrowUp") {
+    headPos.y--;
+    clearAndReselect();
+    headDiv.classList.add("head-up");
+  } else if (e.key === "ArrowDown") {
+    headPos.y++;
+    clearAndReselect();
+    headDiv.classList.add("head-down");
+  };
+};
+
+document.addEventListener("keydown", mechanics);
